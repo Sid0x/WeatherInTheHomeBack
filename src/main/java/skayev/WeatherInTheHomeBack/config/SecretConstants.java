3165233338b093b21a44640ceae5f49f7f696c15
Eugene <40421853+Sid0x@users.noreply.github.com>
2021-11-01 13:47:49 +0200

    Delete SecretConstants.java

--- src/main/java/skayev/WeatherInTheHomeBack/config/SecretConstants.java
+++
@@ -1,6 +0,0 @@
-package skayev.WeatherInTheHomeBack.config;
-
-public interface SecretConstants {
-    String TELEGRAM_TOKEN = "2095313792:AAHK-c1YDFSQw5n594xwJpRjpMF9bNw85iM";
-    String CHAT_ID = "-749481967";
-}
